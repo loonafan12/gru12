26965567501310c13f8431b20fac9ff06ca1cc3f
loonafan12 <110972530+loonafan12@users.noreply.github.com>
2022-08-11 02:39:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ fab = "https://vip-fab-api.myfab.tv/fapi/2/messages/"
 headers = {
    "user-agent": "fab|ios|appstore|1.2.1|15.3.1|iPhone14,3|apple|ko|KR"
 }
-update = fab.json()['messages'][0]
+update = fab['messages'][0]
 
 def thumbnail():
     requests.get(fab, headers=headers)
